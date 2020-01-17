f2aaf426223f6877d8f71e0faf999bc2e13c85bf
BOMI KIM <bomedang@gmail.com>
2020-01-17 11:16:50 +0900

    Add server rest api variables

--- routes.js
+++ routes.js
@@ -3,12 +3,18 @@ const POSTS = "/posts";
 const POST_DETAIL = "/:id";
 
 // Comments
+const COMMENTS ="/comments";
+const COMMENTS_DETAIL ="/:id";
 
 // Search
+const SEARCH ="/search";
 
 const routes = {
   posts: POSTS,
-  postDetail: POST_DETAIL
+  postDetail: POST_DETAIL,
+  comments:COMMENTS,
+  commentsDetail:COMMENTS_DETAIL,
+  search:SEARCH
 };
 
 module.exports = routes;
