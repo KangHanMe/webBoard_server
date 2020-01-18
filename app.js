3432ce03d285fc05c2ae7b4b72aa6ef1cf93d43c
chadonghwa <chadonghwa0004@gmail.com>
2020-01-19 04:53:51 +0900

    Make comments crud mvc pattern

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const morgan = require("morgan");
 
 const routes = require("./routes");
 const postRouter = require("./routers/postRouter");
+const commentRouter = require("./routers/commentRouter");
 
 const app = express();
 
@@ -11,7 +12,9 @@ const PORT = 4000;
 app.use(express.json());
 app.use(morgan("dev"));
 
+// router
 app.use(routes.posts, postRouter);
+app.use(routes.comments, commentRouter);
 
 app.listen(PORT, () =>
   console.log(`Web-board server is listening on port ${PORT}!`)
