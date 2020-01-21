7d46a1b55f6b49fcdbe1a2dfd1e389d5a02d4bde
BOMI KIM <bomedang@gmail.com>
2020-01-21 16:18:41 +0900

    Make comments feature deleted by user

--- controllers/comment/commentdetail.js
+++ controllers/comment/commentdetail.js
@@ -1,5 +1,26 @@
 const { comments } = require("../../models");
 
 module.exports = {
-  delete: (req, res) => {}
+  delete: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { password } = req.body;
+
+      const commentPassword = await comments.findOne({
+        attributes: ["password"],
+        where: { id: id }
+      });
+
+      if (password === commentPassword.dataValues.password) {
+        await comments.destroy({
+          where: { id: id }
+        });
+        res.sendStatus(200);
+      } else {
+        res.status(400).send("wrong password");
+      }
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
 };
